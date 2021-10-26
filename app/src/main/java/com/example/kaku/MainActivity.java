88438edf6a5e5cf7f7451be85d4589026a4c3c71
sharif2124 <azizur15-2124@diu.edu.bd>
2021-10-27 00:25:46 +0600

    update

--- app/src/main/java/com/example/kaku/MainActivity.java
+++ app/src/main/java/com/example/kaku/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.kaku;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               
+            }
+        });
+
     }
 }
